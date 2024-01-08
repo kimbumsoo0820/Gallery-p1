56223bc1af133c23fcb77077a1616c6ad7839843
kimbumsoo0820 <kbs2082@naver.com>
2024-01-08 21:21:37 +0900

    gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./page/Home.jsx";
+import Gallery from "./page/gallery/galleryList.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/gallery" exact element={<Gallery />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/page/gallery/galleryList.jsx
@@ -0,0 +1,20 @@
+import style from './galleryListStyle.module.css'
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+
+export default function Home() {
+    const [category, setCategory] = useState(['All', 'Gallery', 'Info'])
+    return (
+        <div>
+            <div>
+                <div className={style.category}>
+                    {category.map((data) => (
+                        <div >
+                            {data}
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        </div>
+    )
+}

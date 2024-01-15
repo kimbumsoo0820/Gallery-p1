ba7f8cedb773e7120de964865571ccb3d914aa09
kimbumsoo0820 <kbs2082@naver.com>
2024-01-15 18:14:07 +0900

    'axiosDataSetStateInUseEffect'

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
 
 const calMonth = (getNum) => {
     getNum++
@@ -10,37 +11,45 @@ const calMonth = (getNum) => {
     }
 }
 
-const getWeather = (lat, lon, key) => {
+  const getWeather =  (lat, lon, key) =>  {
     let today = new Date()
     let year = today.getFullYear()
     let month = calMonth(today.getMonth())
     let date = today.getDate()
     let dateForm = `${year}${month}${date}`
-    console.log(dateForm)
-    // let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${key}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${dateForm}&base_time=0600&nx=${lat}&ny=${lon}`
-    let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${dateForm}&base_time=0630&nx=${lat}&ny=${lon}`
-    axios.get(url)
+    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${dateForm}&base_time=0630&nx=${lat}&ny=${lon}`
+    return axios.get(url)
     .then((res)=> {
-        console.log(res)
         let skyData = res.data.response.body.items.item.find(x => x.category === 'SKY')
+        console.log('skyData.fcstValue',skyData.fcstValue)
         return skyData.fcstValue
     })
     .catch((e)=> {
         console.log('에러',e)
-        return null
+        return 
     })
-      
-    
   }
 
 export default function Home() {
-
+    
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
     const x = 55
     const y = 127
-    let sky = getWeather(x,y,API_KEY)
+    const [skyData, setSkyData] = useState()
+
+    useEffect( ()=> { // axios호출하고 그 값을 useEffect안에서 setState할 때  async await을 useEffect에 넣어주어야 함
+        async function setData() {
+             setSkyData(await getWeather(x,y,API_KEY))
+        }
+        setData()
+    },[])
+    
 
     return (
-        <div>hello</div>
+        <>
+            <div>hello</div>
+            <div>현재 날씨 정보는{skyData?skyData:'??'}</div>
+
+        </>
     )
 }
